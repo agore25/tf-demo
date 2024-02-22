05cacfb4b0f899b8f293e5ea2a74a5292a20931a
Ankita Gore <136787478+agore25@users.noreply.github.com>
2024-02-22 14:07:49 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "vnet_name" {
 }
 
 variable "vnet_address_space" {
-  description = "The address space that is used by the virtual network"
+  description = "The address space that is used by the virtual network "
   type        = string
   default     = "10.0.0.0/16"
 }
